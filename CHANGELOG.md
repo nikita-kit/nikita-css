5b51910dbc4a6462e15d20dce9e7f0b029f592cd
Benny Görlitz <benny.goerlitz@exozet.com>
2018-12-09 12:05:51 +0100

    added test files for linting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # dev
 
 * removed mixins and extends, these are now part of nikita-generator
+* changed some rules
 * updated `stylelint` to 9.9.0
 * updated `stylelint-order` to 2.0.0
 * replaced `stylelint-find-rules` with `stylelint-find-new-rules`
--- packages/stylelint-config-nikita/index.js
+++ packages/stylelint-config-nikita/index.js
@@ -95,8 +95,7 @@ module.exports = {
         "media-query-list-comma-space-after": "always-single-line",
         "media-query-list-comma-space-before": "never",
         "no-empty-source": true,
-        "no-eol-whitespace": null, //remove option if no-eol-whitespace is fixable
-        // "no-eol-whitespace": [false, { ignore: ["empty-lines"] }], //remove option if no-eol-whitespace is fixable
+        "no-eol-whitespace": true,
         "no-extra-semicolons": true,
         "no-invalid-double-slash-comments": true,
         "no-missing-end-of-source-newline": true,
